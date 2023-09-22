e1332e8d0b029865cb8628a78279f635e1bac39c
jitprosen360 <jitprosen360@gmail.com>
2023-09-22 16:06:49 +0530

    style change three

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -2,7 +2,7 @@ export const STRAPI_API_TOKEN =  process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 
   export const API_URL = process.env.NEXT_PUBLIC_STRAPI_URL || "https://strapiserverbackend.onrender.com";
 
- // export const API_URL = process.env.NEXT_PUBLIC_STRAPI_URL || "http://127.0.0.1:1337";
+//  export const API_URL = process.env.NEXT_PUBLIC_STRAPI_URL || "http://127.0.0.1:1337";
 
  export const API = "https://strapiserverbackend.onrender.com/api";
 
